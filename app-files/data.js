6ab4785aebbd6d4f0be7b80022bda62ad690126d
BIM21X <bim21x@gmail.com>
2025-05-16 12:38:01 +0530

    Add files via upload

---
+++ app-files/data.js
@@ -0,0 +1,97 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-002",
+      "name": "002",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.0535195875937724,
+        "pitch": 0.11290209217697367,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.03415865478588387,
+          "pitch": 0.20875228403797408,
+          "rotation": 6.283185307179586,
+          "target": "0-002"
+        },
+        {
+          "yaw": 0.059355815748217466,
+          "pitch": 0.15789957344844652,
+          "rotation": 0,
+          "target": "1-001"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.059355815748217466,
+          "pitch": 0.15789957344844652,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.059355815748217466,
+          "pitch": 0.15789957344844652,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.059355815748217466,
+          "pitch": 0.15789957344844652,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-001",
+      "name": "001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
